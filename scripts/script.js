6946c548dfaeb3a65df8d1298dc488a9eb00befe
LeonardoP26 <leonardopetroni2001@gmail.com>
2022-07-07 20:05:05 +0200

    messaggio errore e piccole cose

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,9 @@ var lab;
 
 const imgs = document.getElementsByClassName('img');
 cocoSsd.load().then(function (loadedModel) {
-    var j=0;
+    var j = 0;
     model = loadedModel;
-    for(var i=0; i<imgs.length; i++){
+    for (var i = 0; i < imgs.length; i++) {
         model.detect(imgs[i]).then(function (predictions) {
             var p = document.getElementsByClassName("imglabel");
             console.log(i);
@@ -19,116 +19,112 @@ const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
 if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-        video.srcObject = stream;
-        video.addEventListener('loadeddata', predictWebcam);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+        .then(function (stream) {
+            video.srcObject = stream;
+            video.addEventListener('loadeddata', predictWebcam);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 function predictWebcam() {
-    // Now let's start classifying the stream.
-  //  if(video.srcObject.active == true){
-    if(video.srcObject == undefined){
+    //  if(video.srcObject.active == true){
+    if (video.srcObject == undefined) {
         return;
     }
     model.detect(video).then(function (predictions) {
         //console.log(predictions);
         for (let n = 0; n < predictions.length; n++) {
-            
             if (predictions[n].score > 0.9) /* da vedere il fattore */ {
-
                 lab = predictions[n].class;
 
                 var p = document.getElementById("webcamItem");
                 p.innerHTML = lab;
 
-
-               document.getElementById("label").value = lab;
-
-
+                document.getElementById("label").value = lab;
             }
         }
-
     });
-//}
+    //}
     window.requestAnimationFrame(predictWebcam);
-  }
+}
 
 
 
 var ricerca = true;
-function searchLabel(){
+function searchLabel() {
 
-    if(lab){    //scrivere messaggio errore magari in console?
-    //implementare il cambio di pagina
-    var spag = document.getElementById("paginaWebcam")
-    spag.classList.toggle("nascosta");
+    if (lab) {
+        console.log(lab);
 
-    var rpag = document.getElementById("paginaRisultati")
-    rpag.classList.toggle("nascosta");
-    
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-            video.srcObject= undefined;
-        });
-    }
+        var spag = document.getElementById("paginaWebcam")
+        spag.classList.toggle("nascosta");
 
-    console.log(lab);
-    
+        var rpag = document.getElementById("paginaRisultati")
+        rpag.classList.toggle("nascosta");
 
+        var p = document.getElementById("label1");
+        p.innerHTML = lab;
 
-    
-    if(ricerca){
-        fetch('./server/actions.php?label='+lab+'')
-            .then(response =>{
-                return response.json();
-            })
-            .then(result => {
-                for(var i= 0; i < result.length; i++)
-                {
-                    const id = result[i][0];
-                    const source = result[i][1];
-                    const titolodb = result[i][2];
-                    const label = result[i][3];
-                    const descrizione = result[i][4];
-
-                    const griglia = document.getElementById("griglia");
-                    const newQuadro = document.createElement("div");
-                    newQuadro.className = "quadro";
-                    const container = document.createElement("div");
-                    container.className = "container";
-                    const titolo = document.createElement("p");
-                    titolo.className = "imgtitolo";
-                    const desc = document.createElement("p");
-                    desc.className = "desc";
-                    const img = document.createElement("img");
-                    img.crossOrigin = "anonymous";
-                    img.style.width = '100%';
-                    img.className = "img";
-                    img.src = source;
-                    titolo.innerHTML = titolodb;  
-                    desc.innerHTML = descrizione;  
-                    container.appendChild(titolo);
-                    container.appendChild(desc);
-                    newQuadro.appendChild(img);
-                    newQuadro.appendChild(container);
-                    griglia.appendChild(newQuadro);
-                }
-            })
-    }
-    ricerca = !ricerca;
-}
-}
-
-
-function newSearch(){
-        //cambio di pagina
         if (navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function (stream) {
+                    video.srcObject = undefined;
+                });
+        }
+
+
+        if (ricerca) {
+            fetch('./server/actions.php?label=' + lab + '')
+                .then(response => {
+                    return response.json();
+                })
+                .then(result => {
+                    for (var i = 0; i < result.length; i++) {
+                        const id = result[i][0];
+                        const source = result[i][1];
+                        const titolodb = result[i][2];
+                        const label = result[i][3];
+                        const descrizione = result[i][4];
+
+                        const griglia = document.getElementById("griglia");
+                        const newQuadro = document.createElement("div");
+                        newQuadro.className = "quadro";
+                        const container = document.createElement("div");
+                        container.className = "container";
+                        const titolo = document.createElement("p");
+                        titolo.className = "imgtitolo";
+                        const desc = document.createElement("p");
+                        desc.className = "desc";
+                        const img = document.createElement("img");
+                        img.crossOrigin = "anonymous";
+                        img.style.width = '100%';
+                        img.className = "img";
+                        img.src = source;
+                        titolo.innerHTML = titolodb;
+                        desc.innerHTML = descrizione;
+                        container.appendChild(titolo);
+                        container.appendChild(desc);
+                        newQuadro.appendChild(img);
+                        newQuadro.appendChild(container);
+                        griglia.appendChild(newQuadro);
+                    }
+                })
+        }
+        ricerca = !ricerca;
+    } else {
+        const err = document.getElementById("errore");
+        err.innerHTML = "Inquadra in webcam l'oggetto che desideri cercare!";
+        console.log(error);
+    }
+}
+
+
+function newSearch() {
+    //cambio di pagina
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
             .then(function (stream) {
                 video.srcObject = stream;
                 video.addEventListener('loadeddata', predictWebcam);
@@ -136,27 +132,26 @@ function newSearch(){
             .catch(function (error) {
                 console.log(error);
             });
-        }
+    }
+
+    var spag = document.getElementById("paginaWebcam")
+    spag.classList.toggle("nascosta");
+
+    var rpag = document.getElementById("paginaRisultati")
+    rpag.classList.toggle("nascosta");
+
+    const quadro = document.querySelectorAll('.quadro'); //commentare questo se vogliamo vederne di più
+    quadro.forEach(quadro => {
+        quadro.remove();
+    })
 
 
-        var spag = document.getElementById("paginaWebcam")
-        spag.classList.toggle("nascosta");
-    
-        var rpag = document.getElementById("paginaRisultati")
-        rpag.classList.toggle("nascosta");
-        
-        const quadro = document.querySelectorAll('.quadro'); //commentare questo se vogliamo vederne di più
-        quadro.forEach(quadro => {
-            quadro.remove();
-        })
-        
+    ricerca = !ricerca;
 
-        ricerca = !ricerca;
+    lab = "";
+    var p = document.getElementById("webcamItem");
+    p.innerHTML = lab;
 
-        lab = "";
-        var p = document.getElementById("webcamItem");
-        p.innerHTML = lab;
+    document.getElementById("label").value = lab;
 
-        document.getElementById("label").value = lab;
-    
 }
